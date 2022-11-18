1fbbd0b5b2480209a0af09141e2de6b71e06214f
Lowkee1g <59569937+Lowkee1g@users.noreply.github.com>
2022-11-18 12:27:42 +0100

    .

--- Eksamens_Opgaver/Yatzy.c
+++ Eksamens_Opgaver/Yatzy.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
+#define true 1
+#define false 0
 
-
-
+void findLowestFive(int dice, int sizeofdice);
 
 int main(void)
 {
-    int dice[] = {5,1,2,3,4};
+    int dice[] = {5,1,3,4,2};
     int sizeofdice = sizeof(dice)/sizeof(dice[0]);
+    
+    findLowestFive(dice, sizeofdice);
+
+    return 0;
+}
+
+
+void findLowestFive(int dice, int sizeofdice) {
     int tmp = 0;
+    printf("%d", sizeofdice);
 
-
-
-
-
-    for (int i = 0; i <= 5; i++) {
+    for (int i = 1; i <= 5; i++) {
         printf("\n%d || ", i);
-        for (int d = 0; d < sizeofdice+1; d++) {
-            if (i == d) {
-                tmp += d;
-                printf("%d", d);
+        int found = 0;
+        
+        for (int d = 0; d < sizeofdice; d++) {
+            if (i == dice[d] && found == 0) {
+                tmp += dice[d];
+                found = 1;
+                printf("(%d)", dice[d]);
+            } else {
+                printf("%d", dice[d]);
             }
+            
         }
     }
 
     printf("\n%d", tmp);
-
-
-    return 0;
 }
