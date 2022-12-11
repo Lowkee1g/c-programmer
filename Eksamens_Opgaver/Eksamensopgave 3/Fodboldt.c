7666e279b9d3a9a24d92dc166c975855423fdc22
Lowkee1g <59569937+Lowkee1g@users.noreply.github.com>
2022-12-11 14:26:41 +0100

    fscanf fix

--- Eksamens_Opgaver/Eksamensopgave 3/Fodboldt.c
+++ Eksamens_Opgaver/Eksamensopgave 3/Fodboldt.c
@@ -8,7 +8,7 @@
 typedef struct kamp {
     char ugedag[10];
     char dato[10];
-    int klokkeslaet[10];
+    char klokkeslaet[20];
     char hold1[20];
     char hold2[20];
     int resultat1;
@@ -26,6 +26,7 @@ typedef struct hold {
 
 
 void readfile(FILE *fp, Kamp *kampe);
+void printfile(Kamp *kampe);
 
 
 int main(void) {
@@ -36,20 +37,39 @@ int main(void) {
     FILE *fp;
     fp = fopen("kampe-2022-2023.txt", "r");
 
+    //Allocate memory
     kampe = (Kamp *)malloc(kampespillet * sizeof(Kamp));
     hold = (Hold *)malloc(kampespillet * sizeof(Hold));
     
     //Read file
     readfile(fp, kampe);
     
-    for (int i = 0; i < kampespillet; i++) {
-        printf(fp, "%s %s %d:00 %s - %s %d - %d %d", kampe[i].ugedag, kampe[i].dato, kampe[i].klokkeslaet, kampe[i].hold1, kampe[i].hold2, kampe[i].resultat1, kampe[i].resultat2, kampe[i].tilskuertal);
-    }
+    //Print file
+    printfile(kampe);
 }
 
 void readfile(FILE *fp, Kamp *kampe) {
     //Fre     15/07 19.00     FCM - RFC     1 - 1      7359
+    printf("Laeser fil\n");
+
+    
     for (int i = 0; i < kampespillet; i++) {
-        fscanf(fp, "%s %s %d:00 %s - %s %d - %d %d", kampe[i].ugedag, kampe[i].dato, kampe[i].klokkeslaet, kampe[i].hold1, kampe[i].hold2, kampe[i].resultat1, kampe[i].resultat2, kampe[i].tilskuertal);
+        fscanf(fp, "%s %s %s %s - %s %d - %d %d", kampe[i].ugedag, kampe[i].dato, kampe[i].klokkeslaet, kampe[i].hold1, kampe[i].hold2, &kampe[i].resultat1, &kampe[i].resultat2, &kampe[i].tilskuertal);
     }
+    printf("Faerdig med at læse fil\n");
+}
+
+
+
+
+
+
+void printfile(Kamp *kampe) {
+    printf("printer fil\n");
+    printf("%s | %s %s   %s - %s   %d - %d | %d\n", kampe[0].ugedag, kampe[0].dato, kampe[0].klokkeslaet, kampe[0].hold1, kampe[0].hold2, kampe[0].resultat1, kampe[0].resultat2, kampe[0].tilskuertal);
+
+    for (int i = 0; i < kampespillet; i++) {
+        printf("%s | %s %s   %s - %s   %d - %d | %d\n", kampe[i].ugedag, kampe[i].dato, kampe[i].klokkeslaet, kampe[i].hold1, kampe[i].hold2, kampe[i].resultat1, kampe[i].resultat2, kampe[i].tilskuertal);
+    }
+    printf("Faerdig med at printe fil\n");
 }
